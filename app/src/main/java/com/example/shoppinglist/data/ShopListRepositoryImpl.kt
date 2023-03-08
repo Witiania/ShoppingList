22eaa9394a4c03860085882001f5ab5e8dbac9c3
Witiania <Witiania@gmail.com>
2023-03-08 13:38:21 +0300

    Плдключили LiveData подписаолись на нее создали RV

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,14 +1,24 @@
 package com.example.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 
 object ShopListRepositoryImpl:ShopListRepository {
 
 
+    private val shopListLiveData = MutableLiveData<List<ShopItem>>()
     private val shopList = mutableListOf<ShopItem>()
 
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10){
+            val item = ShopItem("Name $i",i,true)
+            addShopItem(item)
+        }
+    }
 
     override fun addShopItem(shopItem: ShopItem) {
         if(shopItem.id == ShopItem.UNDEFINED_ID) {
@@ -16,10 +26,12 @@ object ShopListRepositoryImpl:ShopListRepository {
 
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -33,7 +45,11 @@ object ShopListRepositoryImpl:ShopListRepository {
                 "with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLiveData
+    }
+
+    private fun updateList(){
+        shopListLiveData.value = shopList.toList()
     }
 }
--- app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -1,5 +1,8 @@
 package com.example.shoppinglist.domain
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 interface ShopListRepository {
 
     fun addShopItem(shopItem: ShopItem)
@@ -10,5 +13,6 @@ interface ShopListRepository {
 
     fun getShopItem(shopItemId:Int):ShopItem
 
-    fun getShopList():List<ShopItem>
+    fun getShopList(): LiveData<List<ShopItem>>
+
 }
